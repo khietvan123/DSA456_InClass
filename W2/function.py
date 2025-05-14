b5d5fd10b04088f04f29690831955175a3cbd4e4
Khiet Van <kvphan@myseneca.ca>
2025-05-14 12:34:21 -0400

    big O count

--- W2/function.py
+++ W2/function.py
@@ -7,3 +7,15 @@ def sum(n):
     return rc               # 1
 # T(n) = 1 + (n+1) + n + 1 = 2n + 3
 
+# PROFESSOR:
+def sum(n):
+    rc = 0              # 1
+    for i in range(n):  # n + 1
+        rc += (i+1)     # 2n
+    return rc           # 1
+# T(n) = 1 + n + 1 + 2n + 1 = 3n + 3, therefore T(n) is O(n)
+
+def sum(n):
+    return n*(n+1)//2 # 3
+# T(n) = 3 
+
